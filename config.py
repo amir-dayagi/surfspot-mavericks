343779f34283cee1e9eb72877f4106c4ab4cc044
amir-dayagi <60447229+amir-dayagi@users.noreply.github.com>
2024-03-17 16:37:59 -0700

    moved default secret key to only development config

--- config.py
+++ config.py
@@ -3,12 +3,13 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SECRET_KEY = os.getenv("SECRET_KEY", default="Do I really need this?")
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres@localhost:5432/surfspot'
+    SECRET_KEY = os.getenv("SECRET_KEY", default="Do I really need this?")
 
 class TestingConfig(Config):
     TESTING = True
